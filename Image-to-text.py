569a2092c6f9594ba281f93c60b96cf2582360ae
Oliver Simon Jarvis <201807958@post.au.dk>
2020-04-13 13:09:43 +0200

    Image detection v1

--- Image-to-text.py
+++ Image-to-text.py
@@ -1,9 +1,11 @@
 '''
 Image-to-text library for text detection and extraction. 
 '''
-#import TextExtractor
-#import MemeEstimator
+from TextExtractor import TextExtractor
+from TextExtractor import ImageLabel
+import wordsegment
 from optparse import OptionParser
+from autocorrect import Speller
 import pathlib
 
 class InputError(Exception):
@@ -13,14 +15,34 @@ class InputError(Exception):
         self.message = message
 
 class ImageToText:
-    def __init__(self, input_dir="", extract_text=True, extract_meme_context=False, output_dir=""):
-        pass
+    def __init__(self, input_dir="", extract_text=True, correct_spelling = False, extract_meme_context=False, output_dir=""):
+        TEX = TextExtractor()
+        files = pathlib.Path(input_dir).glob("*")
+        files = [x for x in files if x.is_file()]
+        image_labels = TEX.extract_text(files)
 
+        if correct_spelling:
+            for img in image_labels: 
+                if not len(img.ocr) > 0:
+                    continue
+                check = Speller(lang='en')
+                wordsegment.load()
+                ocr = wordsegment.segment(img.ocr)
+                ocr = " ".join(ocr)
+                ocr = check(ocr)
+                img.ocr = ocr
+        
+        for image in image_labels:
+            image.save_to(output_dir)
+        
 def main():
     parser = OptionParser()
     parser.add_option("-i", "--images", dest="input_directory",
                     action="store", type="string",
                     help="directory of input images", metavar="INPUT")
+    parser.add_option("-s", "--spelling", dest="spelling_b",
+                    action="store_false",
+                    help="Should spelling be corrected", metavar="SPELL")
     parser.add_option("-t", "--text",
                     action="store_false", dest="text_b", default=True,
                     help="output image text")
--- TextExtractor.py
+++ TextExtractor.py
@@ -1,41 +1,90 @@
 import cv2
 import numpy as np
-#import ValidateDictionary
-#import east
+import tempfile
+import pytesseract
 
-class ImageLabel:
-    def __init__(self, image, coordinates):
-        self.image = image
-        self.coordinates = coordinates
+def order_boxes(boxes):
+    ''' method that orders boxes in a left-to-right, top-to-bottom
+        approach. This has it's faults when it comes to memes,
+        but generally is a safe-approach.
+    '''
+    boxes.sort(key=lambda b: b[1])
+    # initially the line bottom is set to be the bottom of the first rect
+    line_bottom = boxes[0][1]+boxes[0][3]-1
+    line_begin_idx = 0
+    for i in range(len(boxes)):
+        # when a new box's top is below current line's bottom
+        # it's a new line
+        if boxes[i][1] > line_bottom:
+            # sort the previous line by their x
+            boxes[line_begin_idx:i] = sorted(boxes[line_begin_idx:i], key=lambda b: b[0])
+            line_begin_idx = i
+        # regardless if it's a new line or not
+        # always update the line bottom
+        line_bottom = max(boxes[i][1]+boxes[i][3]-1, line_bottom)
+    # sort the last line
+    boxes[line_begin_idx:] = sorted(boxes[line_begin_idx:], key=lambda b: b[0])
+    return boxes
 
-def image_thresholding(img, thresh):
-    # load image
-    image = img
-    # resize image
-    image = cv2.resize(image,None,fx=5, fy=5, interpolation = cv2.INTER_CUBIC)
-    # create grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # perform threshold
-    _, mask = cv2.threshold(gray_image, thresh, 255, cv2.THRESH_BINARY)
-    # find contours
-    _, contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # draw black over the contours smaller than 200 - remove unwanted blobs
-    for cnt in contours:
-        # print contoursize to detemine threshold
-        print(cv2.contourArea(cnt))
-        if cv2.contourArea(cnt) < 200:
-            cv2.drawContours(mask, [cnt], 0, (0), -1)
-    return image
+def get_contours(img):
+    # Invert the image
+    img = cv2.bitwise_not(img)
+    # Convert the image to gray scale 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 
-def clip_labels(images):
-    for img in images:
-        pass
-        
+    # Performing OTSU threshold
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
+    cv2.imwrite("thresh1.png", thresh1)  
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
+
+    # Appplying dilation on the threshold image 
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1) 
+    
+    # Finding contours 
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,  
+                                                    cv2.CHAIN_APPROX_NONE)
+    bounding_rectangles = []
+    #Sort rectangles
+    for cnt in contours: 
+        (x, y, w, h) = cv2.boundingRect(cnt)
+        bounding_rectangles.append((x, y, w, h))
+    bounding_rectangles = order_boxes(bounding_rectangles)
+    return bounding_rectangles
+
+def extractText(img):
+    image = cv2.imread(img) 
+    im2 = image.copy() 
+    bounding_rectangles = get_contours(image)
+    ocr_text = ""
+    for rect in bounding_rectangles:
+        (x, y, w, h) = rect
+        # Cropping the text block for giving input to OCR 
+        cropped = im2[y:y + h, x:x + w] 
+        # Apply OCR on the cropped image 
+        text = pytesseract.image_to_string(cropped)
+        # Appending the text into file 
+        if len(text) > 1:
+            ocr_text += text + "\n"
+    return ocr_text
 
 class TextExtractor:
-    def __init__(self, images):
-        self.images = images
-        pass
-        #run EAST
-        #run image_thresholding for each coordinate
-        
+    def __init__(self):
+        self.images = []
+    def extract_text(self, images):
+        for img in images:
+            img_str = img._str
+            if '.ds_store' in img_str.lower():
+                continue
+            text = extractText(img_str)
+            self.images.append(ImageLabel(img, text))
+        return self.images
+
+class ImageLabel:
+    def __init__(self, image, ocr):
+        self.image = image
+        self.ocr = ocr
+    def save_to(self, output_dir=""):
+        output = output_dir + self.image.stem
+        with open(output, "w+") as f:
+            print("Writing file {}".format(self.image.stem))
+            f.write(self.ocr)
